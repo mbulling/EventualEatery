c696cf968fba9e7e3c66bfb32bcfb984d3b70671
David Han <dmh338@cornell.edu>
2021-12-05 16:07:34 -0500

    made calendar

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask.templating import render_template
 import model
 import pickle
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,9 +16,9 @@ def predict():
     if int(dateEntry[1]) < 10:
         dateEntry[1] = '0' + dateEntry[1]
     if int(dateEntry[0]) < 10:
-        dateEntry[1] = '0' + dateEntry[1]
+        dateEntry[0] = '0' + dateEntry[0]
+    dayEntry = datetime.date(day=int(dateEntry[1]), month=int(dateEntry[0]), year=int(dateEntry[2])).strftime('%A %d %B %Y').split(" ")[0]
     dateEntry = dateEntry[2]+"-"+dateEntry[0]+"-"+dateEntry[1]
-    dayEntry = request.args.get('dayEntry')
     foods = model.predict_menu(model.getFood(), model.convert_features(dateEntry, dayEntry))
     print(foods)
     return jsonify({'foods': foods}) 
