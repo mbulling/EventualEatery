211ba12e60303cd51e7605e86a78d80735e6acfb
David Han <dmh338@cornell.edu>
2021-12-05 16:21:43 -0500

    added more dinings

--- get_data.py
+++ get_data.py
@@ -10,13 +10,13 @@ def getFood():
 
         eateries = data["data"]["eateries"]
         for i in range(len(eateries)):
-            if "Becker" in eateries[i]["name"]:
+            if "Dining Room" in eateries[i]["name"] or "Robert" in eateries[i]["name"] or "Oken" in eateries[i]["name"]:
                 for j in range(len(eateries[i]["operatingHours"])):
                     date = eateries[i]["operatingHours"][j]["date"] 
                     dateArr = date.split("-")
                     dayOfWeek = datetime.date(day = int(dateArr[2]), month = int(dateArr[1]), year = int(dateArr[0])).strftime('%A %d %B %Y').split()[0]
                     dateHelper = eateries[i]["operatingHours"][j]
-                    dinnerFood[(date, dayOfWeek)] = []
+                    dinnerFood[(eateries[i]["name"], date, dayOfWeek)] = []
                     for k in range(len(dateHelper["events"])):
                         if dateHelper["events"][k]["descr"] ==  "Dinner":
                             dinnerMenu = dateHelper["events"][k]
@@ -24,7 +24,8 @@ def getFood():
                                 if dinnerMenu["menu"][j]["category"] == "Hot Traditional Station - Entrees" or \
                                 dinnerMenu["menu"][j]["category"] == "Hot Traditional Station - Sides":
                                     for l in range(len(dinnerMenu["menu"][j]["items"])):
-                                        addFood = dinnerFood[(date, dayOfWeek)]
+                                        addFood = dinnerFood[(eateries[i]["name"], date, dayOfWeek)]
                                         addFood.append(dinnerMenu["menu"][j]["items"][l]["item"].strip())
-                                        dinnerFood[(date, dayOfWeek)] = addFood
+                                        dinnerFood[(eateries[i]["name"], date, dayOfWeek)] = addFood
     return dinnerFood
+
