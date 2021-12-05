c13f3d7d928ddbcff2bebd8b314c35eeb63ace86
David Han <dmh338@cornell.edu>
2021-12-05 14:13:15 -0500

    added AJAX

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask.templating import render_template
+import pickle
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')    
+    return render_template('index.html')
+
+@app.route('/predict')
+def predict():
+    dateEntry = request.args.get('dateEntry')
+    dayEntry = request.args.get('dayEntry')
+    return jsonify({'dateEntry': dateEntry, 'dayEntry' : dayEntry}) 
 
 if __name__ == '__main__':
    app.run()
