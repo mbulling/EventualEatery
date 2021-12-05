cef5966aea06ba6d41c8b6fab34c4f900f4a112f
David Han <dmh338@cornell.edu>
2021-12-05 16:24:31 -0500

    ha

--- get_data.py
+++ get_data.py
@@ -16,7 +16,7 @@ def getFood():
                     dateArr = date.split("-")
                     dayOfWeek = datetime.date(day = int(dateArr[2]), month = int(dateArr[1]), year = int(dateArr[0])).strftime('%A %d %B %Y').split()[0]
                     dateHelper = eateries[i]["operatingHours"][j]
-                    dinnerFood[(eateries[i]["name"], date, dayOfWeek)] = []
+                    dinnerFood[(i, date, dayOfWeek)] = []
                     for k in range(len(dateHelper["events"])):
                         if dateHelper["events"][k]["descr"] ==  "Dinner":
                             dinnerMenu = dateHelper["events"][k]
@@ -24,8 +24,8 @@ def getFood():
                                 if dinnerMenu["menu"][j]["category"] == "Hot Traditional Station - Entrees" or \
                                 dinnerMenu["menu"][j]["category"] == "Hot Traditional Station - Sides":
                                     for l in range(len(dinnerMenu["menu"][j]["items"])):
-                                        addFood = dinnerFood[(eateries[i]["name"], date, dayOfWeek)]
+                                        addFood = dinnerFood[(i, date, dayOfWeek)]
                                         addFood.append(dinnerMenu["menu"][j]["items"][l]["item"].strip())
-                                        dinnerFood[(eateries[i]["name"], date, dayOfWeek)] = addFood
+                                        dinnerFood[(i, date, dayOfWeek)] = addFood
     return dinnerFood
 
